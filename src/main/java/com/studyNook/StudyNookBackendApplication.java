466ad721d3e48a0e2a8d017568d6f5288608ad2f
seokyHan <sseok1979@naver.com>
2024-08-10 13:45:13 +0900

    fix: first commit

---
+++ src/main/java/com/studyNook/StudyNookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studyNook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyNookBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyNook/StudyNookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyNook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
