8c13508339f56c6c779446ea4ff0becf6e1345fc
seokyHan <sseok1979@naver.com>
2024-08-22 15:51:27 +0900

    add : propertiesConfiguration class 추가

---
+++ src/main/java/com/studyNook/global/common/config/props/PropertiesConfiguration.java
@@ -0,0 +1,11 @@
+package com.studyNook.global.common.config.props;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(value = {
+
+})
+public class PropertiesConfiguration {
+}
