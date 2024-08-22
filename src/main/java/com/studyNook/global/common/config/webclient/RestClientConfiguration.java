822beb3b370e6cedaf49920cc1f7e783d8603137
seokyHan <sseok1979@naver.com>
2024-08-22 16:49:51 +0900

    add : RestClientConfiguration 추가

---
+++ src/main/java/com/studyNook/global/common/config/webclient/RestClientConfiguration.java
@@ -0,0 +1,9 @@
+package com.studyNook.global.common.config.webclient;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestClientConfiguration {
+
+
+}
