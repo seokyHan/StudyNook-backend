ab867b645f5a10f4e2aee2c54a613a475ed0af7e
seokyHan <sseok1979@naver.com>
2024-08-22 15:58:50 +0900

    add : Redis config 추가

--- src/main/java/com/studyNook/global/common/config/props/PropertiesConfiguration.java
+++ src/main/java/com/studyNook/global/common/config/props/PropertiesConfiguration.java
@@ -1,11 +1,12 @@
 package com.studyNook.global.common.config.props;
 
+import com.studyNook.global.common.config.redis.props.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(value = {
-
+        RedisProperties.class
 })
 public class PropertiesConfiguration {
 }
---
+++ src/main/java/com/studyNook/global/common/config/redis/RedisConfig.java
@@ -0,0 +1,4 @@
+package com.studyNook.global.common.config.redis;
+
+public class RedisConfig {
+}
