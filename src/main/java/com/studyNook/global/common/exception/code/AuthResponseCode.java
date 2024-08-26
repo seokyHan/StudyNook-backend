3d59f9d44016dea41945708a75d60e80c2378f7a
seokyHan <sseok1979@naver.com>
2024-08-26 18:26:37 +0900

    fix : 공통 상태 코드, 메세지 수정

--- src/main/java/com/studyNook/global/common/exception/code/AuthResponseCode.java
+++ src/main/java/com/studyNook/global/common/exception/code/AuthResponseCode.java
@@ -15,16 +15,9 @@ public enum AuthResponseCode implements ResponseCode {
     UNAUTHORIZED("1", HttpStatus.UNAUTHORIZED),
     TOKEN_EXPIRED("2", HttpStatus.UNAUTHORIZED),
     USER_NOT_FOUND("3", HttpStatus.NOT_FOUND),
-    GROUP_USER_NOT_FOUND("4", HttpStatus.NOT_FOUND),
-    UNAUTHORIZED_TOKEN_ERROR("5", HttpStatus.NOT_FOUND),
-    SESSION_UNAUTHORIZED("6", HttpStatus.UNAUTHORIZED),
-    FORBIDDEN("7", HttpStatus.FORBIDDEN),
-    USER_PROJECT_NOT_FOUND("8", HttpStatus.NOT_FOUND),
-    USER_API_KEY_NOT_FOUND("9", HttpStatus.NOT_FOUND),
-    NOT_EQUALS_LOGIN_IP("10", HttpStatus.UNAUTHORIZED),
-    RSA_KEY_CREATE_ERROR("11", HttpStatus.INTERNAL_SERVER_ERROR),
-    RSA_KEY_DECRYPT_ERROR("12", HttpStatus.BAD_REQUEST),
-    AUTH_FAIL("13", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED_TOKEN_ERROR("4", HttpStatus.NOT_FOUND),
+    FORBIDDEN("5", HttpStatus.FORBIDDEN),
+    AUTH_FAIL("6", HttpStatus.UNAUTHORIZED),
     ;
 
     private final String code;
