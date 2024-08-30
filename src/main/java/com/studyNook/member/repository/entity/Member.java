9056dad45bb83df6e0a611afd9867edf4bd81590
seokyHan <sseok1979@naver.com>
2024-08-30 18:20:35 +0900

    add : OAuth2 관련 컬럼 추가

--- src/main/java/com/studyNook/member/repository/entity/Member.java
+++ src/main/java/com/studyNook/member/repository/entity/Member.java
@@ -2,8 +2,10 @@ package com.studyNook.member.repository.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.querydsl.core.annotations.QueryEntity;
+import com.studyNook.oauth2.types.SocialType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -16,21 +18,58 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "ID")
     private Long id;
 
-    @Column(name = "email")
+    @Column(name = "EMAIL")
     private String email;
 
-    @Column(name = "password")
+    @Column(name = "PASSWORD")
     private String password;
 
-    @Column(name = "name")
+    @Column(name = "NAME")
     private String name;
 
+    @Column(name = "NICKNAME")
+    private String nickName;
+
+    @Column(name = "ACTIVATED")
+    private boolean activated;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "member_id", referencedColumnName = "id")
+    @JoinColumn(name = "MEMBER_ID", referencedColumnName = "ID")
     @JsonManagedReference(value = "member-authority")
     private List<MemberAuthority> authorities;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "SOCIAL_TYPE")
+    private SocialType socialType;
+
+    @Column(name = "SOCIAL_ID")
+    private String socialId;
+
+    @Column(name = "IMAGE_URL")
+    private String imageUrl;
+
+    @Builder
+    public Member(String email,
+                  String password,
+                  String name,
+                  String nickName,
+                  boolean activated,
+                  List<MemberAuthority> authorities,
+                  SocialType socialType,
+                  String socialId,
+                  String imageUrl) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.nickName = nickName;
+        this.activated = activated;
+        this.authorities = authorities;
+        this.socialType = socialType;
+        this.socialId = socialId;
+        this.imageUrl = imageUrl;
+    }
 
 }
