56dc92e4cfa3e2fc23c037939c3898e4dbf6d87f
seokyHan <sseok1979@naver.com>
2024-09-01 22:19:45 +0900

    fix : socialType enum 수정

--- src/main/java/com/studyNook/oauth2/types/SocialType.java
+++ src/main/java/com/studyNook/oauth2/types/SocialType.java
@@ -1,7 +1,23 @@
 package com.studyNook.oauth2.types;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+@Getter
 public enum SocialType {
-    GOOGLE,
-    NAVER,
-    KAKAO
+    GOOGLE("google"),
+    NAVER("naver"),
+    KAKAO("kakao");
+
+    private final String registrationId;
+
+    public static SocialType fromRegistrationId(String registrationId){
+        return Arrays.stream(SocialType.values())
+                .filter(type -> type.getRegistrationId().equalsIgnoreCase(registrationId))
+                .findFirst()
+                .orElse(GOOGLE);
+    }
 }
