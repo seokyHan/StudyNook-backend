a5d11b1f97028913150c1b4a24385a8d92f724f9
seokyHan <sseok1979@naver.com>
2024-09-01 22:19:11 +0900

    add : OAuth2 property 추가

--- src/main/java/com/studyNook/global/common/config/PropertiesConfiguration.java
+++ src/main/java/com/studyNook/global/common/config/PropertiesConfiguration.java
@@ -3,6 +3,7 @@ package com.studyNook.global.common.config;
 import com.studyNook.global.security.jwt.props.ExcludeProperties;
 import com.studyNook.global.security.jwt.props.JwtProperties;
 import com.studyNook.global.common.props.RedisProperties;
+import com.studyNook.oauth2.props.OAuth2Properties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(value = {
         RedisProperties.class,
         JwtProperties.class,
-        ExcludeProperties.class
+        OAuth2Properties.class,
+        ExcludeProperties.class,
 })
 public class PropertiesConfiguration {
 }
