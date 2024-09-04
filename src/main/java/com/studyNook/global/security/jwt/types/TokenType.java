d5a4f17dde699db4626aac18c73f45fc7ff3ca41
seokyHan <sseok1979@naver.com>
2024-09-04 17:28:52 +0900

    fix : type 추가

--- src/main/java/com/studyNook/global/security/jwt/types/TokenType.java
+++ src/main/java/com/studyNook/global/security/jwt/types/TokenType.java
@@ -1,6 +1,13 @@
 package com.studyNook.global.security.jwt.types;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum TokenType {
-    ACCESS_TOKEN,
-    REFRESH_TOKEN
+    ACCESS_TOKEN("accessToken"),
+    REFRESH_TOKEN("refreshToken");
+
+    private final String type;
 }
