a30a1be5b9d7243d35ea66cc8fb7a1f565980e6a
seokyHan <sseok1979@naver.com>
2024-10-28 18:35:04 +0900

    add : 공통 code 테이블 entity 추가

---
+++ src/main/java/com/studyNook/common/options/repository/entity/Code.java
@@ -0,0 +1,35 @@
+package com.studyNook.common.options.repository.entity;
+
+import com.querydsl.core.annotations.QueryEntity;
+import com.studyNook.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@QueryEntity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "CODE")
+public class Code extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "code_id")
+    private String codeId;
+
+    @Column(name = "group_id")
+    private String groupId;
+
+    @Column(name = "code_nm")
+    private String codeNm;
+
+    @Column(name = "use_yn")
+    private String useYn;
+
+    @Column(name = "remark")
+    private String remark;
+}
---
+++ src/main/java/com/studyNook/common/options/repository/entity/CodeGroup.java
@@ -0,0 +1,33 @@
+package com.studyNook.common.options.repository.entity;
+
+import com.querydsl.core.annotations.QueryEntity;
+import com.studyNook.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@QueryEntity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "CODE_GROUP")
+public class CodeGroup extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id")
+    private String groupId;
+
+    @Column(name = "group_nm")
+    private String groupNm;
+
+    @Column(name = "use_yn")
+    private String useYn;
+
+    @Column(name = "remark")
+    private String remark;
+
+}
