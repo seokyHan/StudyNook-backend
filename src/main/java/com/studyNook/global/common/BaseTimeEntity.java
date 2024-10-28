9e2e8775d0232caa3e97b5fe71356f1ace95807f
seokyHan <sseok1979@naver.com>
2024-10-28 18:34:41 +0900

    add : auditing baseTimeEntity 추가

---
+++ src/main/java/com/studyNook/global/common/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.studyNook.global.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime regDt;
+
+    @LastModifiedDate
+    private LocalDateTime editDt;
+}
