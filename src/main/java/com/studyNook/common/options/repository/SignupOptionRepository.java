cfb757090dc1526b8af27ddf2e62717905ae730b
seokyHan <sseok1979@naver.com>
2024-10-29 14:36:08 +0900

    fix : class 삭제

--- src/main/java/com/studyNook/common/options/repository/SignupOptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.studyNook.common.options.repository;
-
-import com.studyNook.common.options.repository.entity.SignupOption;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SignupOptionRepository extends JpaRepository<SignupOption, Long> {
-}
